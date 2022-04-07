de434ca9d52a6f9ade58d1bb41f2ceafc83e0826
NicMortelliti <npmortelliti@gmail.com>
2022-04-07 13:49:58 -0700

    Add plant data to plant cards/list

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plantsArrayState, setPlantsArrayState] = useState([]);
+
+  const serverUrl = "http://localhost:6001";
+
+  useEffect(() => {
+    fetch(`${serverUrl}/plants`)
+      .then(results => results.json())
+      .then(data => setPlantsArrayState(data));
+  }, []);
+
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plants={plantsArrayState} />
     </main>
   );
 }

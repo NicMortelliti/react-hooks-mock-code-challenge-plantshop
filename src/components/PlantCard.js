5d97995ee3d65c5cc132f0df86467d01539fcba7
NicMortelliti <npmortelliti@gmail.com>
2022-04-07 15:49:58 -0700

    Add in-stock state/patch handling

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,13 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function PlantCard({ plant: { name, image, price } }) {
+function PlantCard({ plant: { id, name, image, price }, stockClick }) {
+  const [priceStockState, setPriceStockState] = useState(price);
+
+  function handleStockUpdate(e) {
+    fetch(`http://localhost:6001/plants/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ price: 0 }),
+    })
+      .then(r => r.json())
+      .then(updatedItem => setPriceStockState(updatedItem.price));
+  }
+
   return (
     <li className="card">
       <img src={image} alt={name} />
       <h4>{name}</h4>
-      <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      <p>Price: {priceStockState}</p>
+      {priceStockState ? (
+        <button className="primary" onClick={handleStockUpdate}>
+          In Stock
+        </button>
       ) : (
         <button>Out of Stock</button>
       )}
